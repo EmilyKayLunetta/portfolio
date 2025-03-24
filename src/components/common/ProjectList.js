0a8f5d323696af89c758216971198cdb66f83052
Christopher Lunetta <cslunetta@outlook.com>
2025-03-24 12:58:34 -0500

    display images for art works

--- src/components/common/ProjectList.js
+++ src/components/common/ProjectList.js
@@ -1,20 +1,41 @@
-import React from "react";
-import { Grid2 as Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Grid2 as Grid, Card, CardMedia, CardContent, Typography, Dialog, DialogContent } from "@mui/material";
 
 function ProjectList({ items, title }) {
+    const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+
+    const handleClickOpen = (image) => {
+        setSelectedImage(image);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setSelectedImage("");
+    };
+
     return (
-        <>
-            <Typography variant="h4" align="center" sx={{ mt: 5 }}>
+        <div>
+            <Typography variant="h4" gutterBottom>
                 {title}
             </Typography>
             <Grid container spacing={4} justifyContent="center">
                 {items.map((item) => (
-                    <Grid key={item.id} xs={12} sm={6} md={4}>
-                        <Card>
-                            <CardMedia component="img" height="200" image={item.image} alt={item.title} />
+                    <Grid item key={item.id} xs={12} sm={6} md={4}>
+                        <Card sx={{ maxWidth: 345 }} onClick={() => handleClickOpen(item.image)}>
+                            <CardMedia
+                                component="img"
+                                height="300"
+                                image={item.image}
+                                alt={item.title}
+                                style={{ objectFit: "cover", cursor: "pointer" }}
+                            />
                             <CardContent>
-                                <Typography variant="h6">{item.title}</Typography>
-                                <Typography variant="body2" color="textSecondary">
+                                <Typography variant="h5" component="div">
+                                    {item.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
                                     {item.description}
                                 </Typography>
                             </CardContent>
@@ -22,7 +43,12 @@ function ProjectList({ items, title }) {
                     </Grid>
                 ))}
             </Grid>
-        </>
+            <Dialog open={open} onClose={handleClose} maxWidth="md">
+                <DialogContent>
+                    <img src={selectedImage} alt="Expanded view" style={{ width: "100%" }} />
+                </DialogContent>
+            </Dialog>
+        </div>
     );
 }
 
