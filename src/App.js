d5abd18d27695c4cc031d7e7b3d542032ee86cb6
Christopher Lunetta <cslunetta@outlook.com>
2025-03-25 10:24:04 -0500

    refactor: simplify App component structure by integrating Navbar directly into Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/common/Navbar';
 import AppRoutes from './routes';
 
 function App() {
-  const location = useLocation();
-  const showNavbar = location.pathname !== '/portfolio';
-
-  return (
-    <>
-      {showNavbar && <Navbar />}
-      <AppRoutes />
-    </>
-  );
-}
-
-function AppWithRouter() {
   return (
     <Router>
-      <App />
+      <Navbar />
+      <AppRoutes />
     </Router>
   );
 }
 
-export default AppWithRouter;
+export default App;
